8f0b55822a8ada6aa0da5b313e36e43c8c2f21d5
Shatrughan Kumar <123942835+shatrukumar47@users.noreply.github.com>
2024-01-27 19:12:52 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,43 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
 //Middlewares
 app.use(express.json());
 app.use(cors());
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: ["https://chantlo.netlify.app/", "https://chantlo.netlify.app", "chantlo.netlify.app", "www.chantlo.netlify.app"],
+});
+
+let userData = [];
+
 io.on("connection", (socket) => {
-  console.log("New user : ", socket);
+
+  socket.on('join-room', (data)=>{
+    const {roomNo} = data;
+    userData.push({...data, socketID: socket.id})
+    socket.join(roomNo)
+    socket.to(roomNo).emit("new-person", data);
+  })
+
+  socket.on("send-message", (data)=>{
+    socket.to(data?.roomNo).emit("received-message", data);
+  })
+
+  socket.on("disconnect", ()=>{
+    let leftUserSocketID = socket.id;
+    let leftUserDetails = userData.find((item)=> item.socketID = leftUserSocketID)
+    socket.to(leftUserDetails?.roomNo).emit('left-room', {...leftUserDetails})
+    userData = userData.filter((item)=> item.socketID !== leftUserSocketID)
+  })
+
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Chantlo Backend");
 });
 
 server.listen(8080, () => {
